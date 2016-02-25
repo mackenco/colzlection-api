51f1c675e17fc09af11201bcbdd3bf48d734a132
Colin <colinwmackenzie@gmail.com>
2016-02-24 22:33:58 -0500

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var url = require('url');
+var fs = require('fs');
+var contents = fs.readFileSync('sites.json');
+var sites = JSON.parse(contents);
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json()); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.json(sites);
+});
+
+app.post('/', function(req, res) {
+  var entry = {};
+  entry.url = req.body.url;
+  entry.host = url.parse(req.body.url).hostname;
+  entry.finished = req.body.finished;
+  entry.date = new Date();
+  sites.push(entry);
+  fs.writeFile('sites.json', JSON.stringify(sites, null, 4), function(e) {
+    e ? console.log(e) : res.json(entry);
+  });
 });
 
 app.listen(app.get('port'), function() {
