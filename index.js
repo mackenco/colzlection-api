6d70d4530e8a5a4b798c03b5869eec9cad28604b
Colin <colinwmackenzie@gmail.com>
2016-02-25 22:32:17 -0500

    added mongoose

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var url = require('url');
-var pg = require('pg');
+var mongoose = require('mongoose');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -15,17 +15,69 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+var uristring = 
+  process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/HelloMongoose';
+
+var theport = process.env.PORT || 5000;
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+var articleSchema = new mongoose.Schema({
+  url: { type: String },
+  host: { type: String },
+  finished: { type: Boolean },
+  title: { type: String }
+});
+
+var Article = mongoose.model('Articles', articleSchema);
+
+var a1 = new Article ({
+  url: 'https://www.washingtonpost.com/world/national-security/justice-scalia-spent-his-last-hours-with-members-of-this-secretive-society-of-elite-hunters/2016/02/24/1d77af38-db20-11e5-891a-4ed04f4213e8_story.html',
+  host: 'washingtonpost.com',
+  finished: false,
+  title: 'Justice Scalia spent his last hours with members of this secretive society of elite hunters'
+});
+a1.save(function (err) {if (err) console.log ('Error on save!');});
+
+var a2 = new Article ({
+  url: 'http://deadspin.com/dexter-fowlers-agent-rips-into-the-orioles-and-the-base-1761381916',
+  host: 'deadspin.com',
+  finished: true,
+  title: "Dexter Fowler's Agent Rips Into The Orioles And The Baseball Media"
+});
+a2.save(function (err) {if (err) console.log ('Error on save!');});
+
+var a3 = new Article ({
+  url: 'http://deadspin.com/the-warriors-are-the-best-team-ever-and-they-can-eat-s-1761305524',
+  host: 'deadspin.com',
+  finished: false,
+  title: 'The Warriors Are The Best Team Ever, And They Can Eat Shit'
+});
+a3.save(function (err) {if (err) console.log ('Error on save!');});
+
+var a4 = new Article ({
+  url: 'http://www.avclub.com/article/what-will-win-and-what-should-win-2016-oscars-232776',
+  host: 'avclub.com',
+  finished: false,
+  title: 'What will win, and what should win, at the 2016 Oscars'
+});
+a4.save(function (err) {if (err) console.log ('Error on save!');});
+
 app.get('/', function(req, res) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err) {
-        console.error(err);
-        res.send("Error " + err); 
-      } else {
-        res.render('pages/db', { results: result.rows }); 
-      } 
-    }); 
+  Article.find({}).exect(function(err, result) {
+    if (!err) {
+      res.json(result); 
+    } else {
+      res.end('Error:' + err);
+    } 
   });
 });
 
